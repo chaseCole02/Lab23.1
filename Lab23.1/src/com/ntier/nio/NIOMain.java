e03d1b713648be218fb252a079c8b057dde317d8
scoot <scoot@10.0.0.61>
2020-04-06 11:43:36 -0400

    Fresh code from the Lab

---
+++ Lab23.1/src/com/ntier/nio/NIOMain.java
@@ -0,0 +1,30 @@
+package com.ntier.nio;
+
+import java.io.IOException;
+import java.nio.file.DirectoryStream;
+import java.nio.file.FileVisitor;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+public class NIOMain {
+
+	public static void main(String[] args) {
+		Path pth = Paths.get("C:\\Users\\scoot\\OneDrive\\Desktop\\Week10Ex\\eclipse-Workspace\\Lab23.1");
+		try (DirectoryStream<Path> ds = Files.newDirectoryStream(pth, "a*")){
+			for(Path currentEntry : ds ) {
+				System.out.println("Entry:" + currentEntry.getFileName());
+			}
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		try {
+			FileVisitor<Path> fv = new ShowJavaFiles();
+			Files.walkFileTree(pth, fv);
+		}catch(IOException fe){
+			fe.printStackTrace();
+		}
+	}
+
+}
